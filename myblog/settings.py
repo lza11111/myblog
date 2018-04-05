0a5dd864ef35cc60907cc3f8feef39271d4f0412
lza11111 <12729828@qq.com>
2018-04-05 15:57:36 +0800

    change setting
    ready to publish

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ehsa9$oo)5%#h*w&ya68z^r%^d=g%5^zx6p#)%(c!4^3!t8(jg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.lza11111.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
