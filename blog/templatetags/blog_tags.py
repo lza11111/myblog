5a2e62eb402923169c066f26aecae899a69eacef
lza11111 <12729828@qq.com>
2018-04-10 14:01:18 +0800

    add time at pagebottom and add edit button

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.db.models.aggregates import Count
 from ..models import Post,Category,Tag
+import datetime
 
 register = template.Library()
 
@@ -19,3 +20,7 @@ def get_category():
 @register.simple_tag
 def get_tags():
     return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+@register.simple_tag
+def get_nowtime():
+    return datetime.datetime.now().strftime("- %b %d %Y %H:%M:%S") #.format(y='年',m='月',d='日')
